1494efe6094e60a67cad2646303d7da4f60ed325
Vals098 <valeria.farinosi@gmail.com>
2026-06-19 08:54:06 +0200

    Initial commit

---
+++ src/main/java/valeriafarinosi/Application.java
@@ -0,0 +1,13 @@
+package valeriafarinosi;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4-w3-d5");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
