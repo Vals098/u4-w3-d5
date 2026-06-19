6bf59d2978a05f1c414b0407796eed761690e3f5
Vals098 <valeria.farinosi@gmail.com>
2026-06-19 10:13:11 +0200

    add: Elementi abstract class

---
+++ src/main/java/valeriafarinosi/entities/Elementi.java
@@ -0,0 +1,71 @@
+package valeriafarinosi.entities;
+
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "elementi")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo_elemento")
+public abstract class Elementi {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_elemento")
+    private UUID idElemento;
+
+    @Column(name = "codice_isbn")
+    private String codiceISBN;
+
+    @Column(name = "titolo")
+    private String titolo;
+
+    @Column(name = "anno_pubblicazione")
+    private int annoPubblicazione;
+
+    @Column(name = "numero_pagine")
+    private int numeroPagine;
+
+    protected Elementi() {
+    }
+
+    public Elementi(String codiceISBN, String titolo, int annoPubblicazione, int numeroPagine) {
+        this.codiceISBN = codiceISBN;
+        this.titolo = titolo;
+        this.annoPubblicazione = annoPubblicazione;
+        this.numeroPagine = numeroPagine;
+    }
+
+    public UUID getIdElemento() {
+        return idElemento;
+    }
+
+    public String getCodiceISBN() {
+        return codiceISBN;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    @Override
+    public String toString() {
+        return "Elementi{" +
+                "idElemento=" + idElemento +
+                ", codiceISBN='" + codiceISBN + '\'' +
+                ", titolo='" + titolo + '\'' +
+                ", annoPubblicazione=" + annoPubblicazione +
+                ", numeroPagine=" + numeroPagine +
+                '}';
+    }
+}
